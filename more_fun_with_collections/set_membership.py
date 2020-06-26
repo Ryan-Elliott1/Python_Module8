90f03c9b5fdbea8aba0ac67632604bb240864030
Ryan-Elliott1 <60247044+Ryan-Elliott1@users.noreply.github.com>
2020-06-26 13:06:33 -0500

    Set assignment tests pass

--- more_fun_with_collections/set_membership.py
+++ more_fun_with_collections/set_membership.py
@@ -2,12 +2,21 @@
 Program: set_membership
 Author: Ryan Elliott
 Last date modified: 06/26/2020
+This program returns true or false if the given value is in the users set
+Input set and value to look for
+Outputs true or false
 """
 
 
 def in_set(my_set, look):
-    pass
+    """Returns true is the element is in the set or false if not
+    :param my_set the users set
+    :param look the element to search for
+    :returns true or false
+    """
+    return look in my_set
 
 
 if __name__ == '__main__':
-    in_set()
+    my_sets = {1, 4, 2, 3, 4}
+    print(in_set(my_sets, 3))
