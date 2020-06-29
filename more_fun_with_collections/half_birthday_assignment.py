7abaa781b949beebb5ad02e208ac3f5f8ac15fc4
Ryan-Elliott1 <60247044+Ryan-Elliott1@users.noreply.github.com>
2020-06-28 21:09:00 -0500

    half birthday tests pass

--- more_fun_with_collections/half_birthday_assignment.py
+++ more_fun_with_collections/half_birthday_assignment.py
@@ -2,10 +2,23 @@
 Program: half_birthday_assignment.py
 Author: Ryan Elliott
 Last date modified: 06/28/2020
+This program calculates the half birthday of a given date
+Input birthday
+Outputs the half birthday
 """
 from datetime import datetime, timedelta
 from dateutil.relativedelta import *
 
 
 def half_birthday(birthday_in):
-    pass
+    """Calculates the half birthday of a given date
+    :param birthday_in, the users birthday
+    :return The users half birthday
+    """
+    birthday = datetime.strptime(birthday_in, '%d/%m/%Y')
+    birthday = birthday + relativedelta(months=-6)
+    return str(birthday)
+
+
+if __name__ == '__main__':
+    print((half_birthday('8/8/2020')))
