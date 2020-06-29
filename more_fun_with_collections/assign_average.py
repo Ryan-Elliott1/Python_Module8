f0f2c1e02294156d0d271d58274e533d6fd4411e
Ryan-Elliott1 <60247044+Ryan-Elliott1@users.noreply.github.com>
2020-06-28 19:53:13 -0500

    Final test key pass with default

--- more_fun_with_collections/assign_average.py
+++ more_fun_with_collections/assign_average.py
@@ -2,21 +2,38 @@
 Program: assign_average.py
 Author: Ryan Elliott
 Last date modified: 06/28/2020
+This program uses a dictionary to replicate a switch-case in python
+Input a choice from 1-6 to get a return or anything else for a default
+Outputs choice or default
 """
 
 
 def a():
     return "You chose a"
+
+
 def A():
     return "You chose A"
+
+
 def b():
     return "You chose b"
+
+
 def B():
     return "You chose B"
+
+
 def c():
     return "You chose c"
+
+
 def C():
     return "You chose C"
+
+
+def default():
+    return "Invalid choice"
 
 
 switch = {
@@ -30,8 +47,12 @@ switch = {
 
 
 def switch_average(choice):
-    return switch.get(choice)
+    """Returns a statement using the dictionary and functions depending on the input
+    :param choice the users selection
+    :returns the users choice
+    """
+    return switch.get(choice, default())
 
 
 if __name__ == '__main__':
-    print(switch_average(1))
+    print(switch_average(0))
