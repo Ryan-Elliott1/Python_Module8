3034c6d76421739aa7e4450067ada8b6f5ba6b93
Ryan-Elliott1 <60247044+Ryan-Elliott1@users.noreply.github.com>
2020-06-28 19:39:46 -0500

    test key 1 and 2 pass

--- more_fun_with_collections/assign_average.py
+++ more_fun_with_collections/assign_average.py
@@ -5,5 +5,21 @@ Last date modified: 06/28/2020
 """
 
 
+def a():
+    return "You chose a"
+def A():
+    return "You chose A"
+
+
+switch = {
+    1: a(),
+    2: A()
+}
+
+
 def switch_average(choice):
-    pass
+    return switch.get(choice)
+
+
+if __name__ == '__main__':
+    print(switch_average(1))
